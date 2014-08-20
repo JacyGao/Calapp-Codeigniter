d3d26a4fd54ea279629955762fd225ab4404d6f2
Jacy Gao <aus.jacy@gmail.com>
2014-08-21 09:09:48 +1000

    models

--- application/models/appointmentm.php
+++ application/models/appointmentm.php
@@ -1,7 +1,19 @@
 <?php
 
-class Appointment extends MY_Model
+class Appointmentm extends MY_Model
 {
+    /**
+     * Model Configuration
+     */
+
+    public $_table = 'appointment';
+    public $primary_key = 'id';
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Constructor
+     */
 
     public function __construct()
     {
@@ -9,4 +21,44 @@ class Appointment extends MY_Model
 
     }
 
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Fetch appointment by ID
+     *
+     * @author		Jacy Gao
+     * @return		void
+     * @param		id <appointment.id>
+     */
+
+    public function get($id)
+    {
+        $this->db->select('*')
+            ->where('id', $id);
+
+        $q = $this->db->get($this->_table);
+
+        return ($q->num_rows() > 0) ? $q->row() : false;
+    }
+
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Fetch Services based on Calendar ID
+     *
+     * @author		Jacy Gao
+     * @return		void
+     * @param		$cid <appointment.calendar_id>
+     */
+
+    public function get_by_calendar($cid)
+    {
+        $q = $this->db->select('*')
+            ->where('calendar_id', $cid)
+            ->get($this->_table);
+
+        return $q->result();
+    }
 }
--- application/models/calendarm.php
+++ application/models/calendarm.php
@@ -1,7 +1,19 @@
 <?php
 
-class Calendar extends MY_Model
+class Calendarm extends MY_Model
 {
+    /**
+     * Model Configuration
+     */
+
+    public $_table = 'calendar';
+    public $primary_key = 'id';
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Constructor
+     */
 
     public function __construct()
     {
@@ -9,4 +21,44 @@ class Calendar extends MY_Model
 
     }
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * Fetch calendar by ID
+     *
+     * @author		Jacy Gao
+     * @return		void
+     * @param		id <calendar.id>
+     */
+
+    public function get($id)
+    {
+        $this->db->select('*')
+            ->where('id', $id);
+
+        $q = $this->db->get($this->_table);
+
+        return ($q->num_rows() > 0) ? $q->row() : false;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Fetch all calendars
+     *
+     * @author		Jacy Gao
+     * @return		void
+     * @param		void
+     */
+
+    public function get_all()
+    {
+
+        $this->db->select('*')
+            ->order_by('id', 'asc');
+
+        $q = $this->db->get($this->_table);
+
+        return $q->result();
+    }
 }
